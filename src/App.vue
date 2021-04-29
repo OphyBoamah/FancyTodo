8eda5e71b7b684ba1378eb8054a0177fc5252d0c
Ophy Boamah <ophybee8@gmail.com>
2021-04-29 17:35:51 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <h1>My Todo List</h1>
+    <form @submit.prevent="addNewTodo">
+      <input type="text" placeholder="Enter a todo here" v-model="newTodo">
+      <button type="submit">Save</button>
+    </form>
+  <div class="todos" v-for="todo in todos" :key="todo.id">
+    <p>{{todo.task}}</p>
+  </div>
+    {{newTodo.task}}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo:"",
+      newId:6,
+      todos : [
+        {
+          id:1,
+          task:"Wake up"
+        },
+        {
+          id:2,
+          task:"Have morning devotion"
+        },
+        {
+          id:3,
+          task:"Perform house chores"
+        },
+        {
+          id:4,
+          task:"Have breakfast"
+        },
+        {
+          id:5,
+          task:"Read a book"
+        },
+      ]
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.todos.push({id:this.newId, task:this.newTodo})
+          this.newTodo = ""
+          this.newId++
+
+    }
   }
 }
 </script>
@@ -24,3 +66,12 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+<!--<form @submit.prevent="addNewTodo">-->
+<!--<input type="text" placeholder="Enter a todo" v-model="newTodo">-->
+<!--<button type="submit">Save</button>-->
+<!--</form>-->
+<!--<div id="todos" v-for="todo in todos" :key="todo.id">-->
+<!--{{todo.task}}-->
+<!--</div>-->
